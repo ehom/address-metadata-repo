0fd4ef4d7116723914b56f0e15e2433db957a4f8
ehom <erwin.hom@puppet.com>
2020-10-09 14:49:52 -0700

    add support for address entry

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -67,7 +67,7 @@ function CountrySelector(properties) {
 function updateAddressFormat(countryCode) {
   console.debug("updateAddressFormat: ", countryCode);
   ReactDOM.render(<AddressFormat countryCode={countryCode} />, document.getElementById('addressFormat'));
-  ReactDOM.render(<AddressEntryForm countryCode={countryCode} />, document.getElementById('addressEntry'));
+  ReactDOM.render(<AddressEntryForm countryCode={countryCode} address={new Address()}/>, document.getElementById('addressEntry'));
 }
 
 function Address() {
@@ -85,22 +85,83 @@ function AddressFormat(properties) {
     
   const result = formatted.map((line) => {
     return (
-      <React.Fragment>
-        <span>{line}</span><br/>
+      <React.Fragment>{line}<br/>
       </React.Fragment>
     );
   });
 
   return (
-    <React.Fragment>Address Format for {properties.countryCode}
+    <React.Fragment>
       <div>{result}</div>
     </React.Fragment>
   );
 }
 
 function AddressEntryForm(properties) {
+  const parts = AddressFormatter(properties.countryCode).formatToParts(properties.address);
+  console.debug(parts);
+
+  const output = parts.map((part) => {
+    let temp;
+
+    switch(part.type) {
+    case 'name':
+      return (
+        <React.Fragment>
+          <p><input type="text" className="form-control" placeholder="Name" /></p>
+        </React.Fragment>
+      );
+    case 'organization':
+      return (
+        <React.Fragment>
+          <p><input type="text" className="form-control" placeholder="Organization" /></p>
+        </React.Fragment>
+      );
+    case 'address':
+      return (
+        <React.Fragment>
+          <p><input type="text" className="form-control" placeholder="Address" /></p>
+        </React.Fragment>
+      );
+    case 'city':
+      return (
+        <React.Fragment>
+          <p><input type="text" className="form-control" placeholder="City" /></p>
+        </React.Fragment>
+      );
+    case 'sublocality':
+      temp = ehom.i18n.addressData[properties.countryCode].sublocality_name_type || ehom.i18n.addressData['ZZ'].sublocality_name_type;
+      return (
+        <React.Fragment>
+          <p><input type="text" className="form-control" placeholder={temp} /></p>
+        </React.Fragment>
+      );
+    case 'state':
+      temp = ehom.i18n.addressData[properties.countryCode].state_name_type || ehom.i18n.addressData['ZZ'].state_name_type;
+      return (
+        <React.Fragment>
+          <p><input type="text" className="form-control" placeholder={temp} /></p>
+        </React.Fragment>
+      );
+    case 'postalCode':
+      return (
+        <React.Fragment>
+          <p><input type="text" className="form-control" placeholder="Postal code" /></p>
+        </React.Fragment>
+      );
+    case 'sortCode':
+      return (
+        <React.Fragment>
+          <p><input type="text" className="form-control" placeholder="Sort code" /></p>
+        </React.Fragment>
+      );
+    default:
+      return <React.Fragment></React.Fragment>;
+    }
+  });
+
   return (
-    <div>TODO</div>
+    <div>{output}</div>
   );
 }
 
@@ -108,6 +169,8 @@ function AddressEntryForm(properties) {
 function AddressFormatter(countryCode) {
   return {
     format: function(object) {
+      const brackets = (s) => `[${s}]`;
+     
       // Read the format string from the locale data
 
       console.debug("format: ", ehom.i18n.addressData[countryCode].fmt);
@@ -123,22 +186,56 @@ function AddressFormatter(countryCode) {
                       .replace('%X', '[sort code]')
 
       const locality_name_type = addressData.locality_name_type ?
-        addressData['locality_name_type'] : defaultData.locality_name_type ;
-      output = output.replace('%C', `[${locality_name_type}]`);
+        addressData.locality_name_type : defaultData.locality_name_type; 
+      output = output.replace('%C', brackets(locality_name_type));
 
-      output = output.replace("%D", addressData.sublocality_name_type ?
-        '[' + addressData.sublocality_name_type + ']' : 
-        '[' + defaultData.sublocality_name_type + ']' );
+      output = output.replace("%D",
+          brackets(
+            addressData.sublocality_name_type ? 
+              addressData.sublocality_name_type: 
+              defaultData.sublocality_name_type
+          )
+      );
 
-      output = output.replace("%S", addressData['state_name_type'] ?
-        '[' + addressData.state_name_type + ']' : 
-        '[' + defaultData.state_name_type + ']' );
+      output = output.replace("%S", 
+        addressData['state_name_type'] ?
+          brackets(addressData.state_name_type):
+          brackets(defaultData.state_name_type)
+      );
 
-      output = output.replace("%Z", addressData.zip_name_type ?
-          `[${addressData.zip_name_type} code]`:
-          `[${defaultData.zip_name_type} code]`);
+      let result = addressData.zip_name_type ? addressData.zip_name_type : defaultData.zip_name_type;
+      output = output.replace("%Z", brackets(`${result} code`));
 
       return output.split("\n");
+    },
+    formatToParts: function(object) {
+      let fmt = ehom.i18n.addressData[countryCode].fmt;
+      let parts = fmt.match(/%[N,O,A,D,C,S,Z,X]/g);
+
+      console.log(parts);
+      let result = parts.map((part) => {
+        switch(part) {
+        case '%N':
+          return {type: 'name', value: '' };
+        case '%O':
+          return {type: 'organization', value: '' };
+        case '%C':
+          return {type: 'city', value: '' };
+        case '%D':
+          return {type: 'sublocality', value: '' };
+        case '%S':
+          return {type: 'state', value: '' };
+        case '%Z':
+          return {type: 'postalCode', value: '' };
+        case '%X':
+          return {type: 'sortCode', value: '' };
+        case '%A':
+          return {type: 'address', value: '' };
+        default:
+          return {type: 'none', value: '' };
+        }
+      });
+      return result;
     }
   };
 }
