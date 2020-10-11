d0d2c8791195592ebad5a990a46574d7545c2321
ehom <erwin.hom@puppet.com>
2020-10-10 20:17:04 -0700

    add Local Format label

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -16,11 +16,13 @@ function App(properties) {
         </div>
       </div>
       <div className="container mb-3">
-        <h3>output forms</h3>
+        <h4>output forms</h4>
+        <p className="mb-2 text-muted">Local format</p>
         <div id="addressFormat"></div>
       </div>
       <div className="container">
-        <h3>entry forms</h3>
+        <h4>input forms</h4>
+        <p className="mb-2 text-muted">Local format</p>
         <div id="addressEntry"></div>
       </div>
     </React.Fragment>
